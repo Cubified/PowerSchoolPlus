a6691764406b06e1846dda832234d04494c69557
CubeDoodl <andrewrussell2002@gmail.com>
2017-11-07 13:15:02 -0500

    Re-enable inactivity timer

--- src/InitSmallFixes.js
+++ src/InitSmallFixes.js
@@ -2,6 +2,7 @@ const utils = require('./lib/PowerSchoolUtil.js');
 
 function InitSmallFixes(){
 	if(utils.IsLoginScreen()){
+		// Disable strange "scaling down" bug
 		let style = document.createElement('style');
 		style.innerHTML = `*:not(.waves-effect):not(.waves-rippling):not(.waves-ripple){transition:none !important;}`;
 		document.head.appendChild(style);
