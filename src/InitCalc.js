a50f2495a4d5791ccf9bcf827915c9a9c69ad380
CubeDoodl <andrewrussell2002@gmail.com>
2017-12-02 20:28:24 -0500

    Add (very experimental) support for total point grade calculation

--- src/InitCalc.js
+++ src/InitCalc.js
@@ -40,6 +40,15 @@ const calcWeightedMean = (array) => {
    const n = sumAllInArray(t);
    return(n / sumAllInArray(w));
 };
+const calcUnweightedMean = (array) => {
+   let totalGrade = 0,
+      totalWeight = 0;
+   array.forEach((e) => {
+      totalGrade += e.grade;
+      totalWeight += e.weight;
+   });
+   return(totalGrade / totalWeight) * 100;
+};
 
 const getNumbersInString = (str) => {
    let out = '';
@@ -89,7 +98,207 @@ const determineWeight = (category) => {
    }
 };
 
+// Calculation strategy generic
+class CalculationStrategy {
+   constructor(calculationFunc, weightHeuristic, gradeHeuristic, gradeDisplayFunc) {
+      this.calculate = calculationFunc;
+      this.determineWeight = weightHeuristic;
+      this.determineGrade = gradeHeuristic;
+      this.determineGradeDisplay = gradeDisplayFunc;
+   }
+}
+// Specific calculation strategies
+const WeightedCalculator = new CalculationStrategy((grades) => {
+   return calcWeightedMean(grades).toFixed(1);
+}, (el) => {
+   return determineWeight(el.children[1].innerText);
+}, (el) => {
+   return parseFloat(getNumbersInString(el.children[5].innerText))
+}, (weight, grade) => {
+   const date = new Date();
+   return `
+		<td>${((date.getMonth() < 9 ? '0' : '') + (date.getMonth() + 1)) + '/' + date.getDate() + '/' + date.getUTCFullYear()}</td>
+		<td>${weight + '%'}</td>
+		<td>New Grade (Click to Disable)</td>
+		<td></td>
+		<td align="center">${parseFloat(grade.toFixed(2)) + '/100'}</td>
+		<td align="center">${parseFloat(grade.toFixed(2)) + '%'}</td>
+		<td align="center">${calculateLetterGrade(grade)}</td>
+		<td width="19"><img class="indicator" style="display:none" src="/images/icon_check.gif"></td>
+		<td width="19"><img class="indicator" style="display:none" src="/images/icon_late.gif"></td>
+		<td width="19"><img class="indicator" style="display:none" src="/images/icon_missing.gif"></td>
+		<td width="19"><img class="indicator" style="display:none" src="/images/icon_exempt.gif"></td>
+		<td width="19"><img class="indicator" style="display:none" src="/images/icon_excluded.gif"></td></tr>
+	`;
+});
+const TotalPointCalculator = new CalculationStrategy((grades) => {
+   return calcUnweightedMean(grades).toFixed(1);
+}, (el) => {
+   return parseFloat(el.children[4].innerText.split('/')[1]);
+}, (el) => {
+   return parseFloat(el.children[4].innerText.split('/')[0]);
+}, (weight, grade) => {
+   const date = new Date();
+   return `
+   		<td>${((date.getMonth() < 9 ? '0' : '') + (date.getMonth() + 1)) + '/' + date.getDate() + '/' + date.getUTCFullYear()}</td>
+   		<td>${weight} Points</td>
+   		<td>New Grade (Click to Disable)</td>
+   		<td></td>
+   		<td align="center">${parseFloat(grade.toFixed(2))}/${weight}</td>
+   		<td align="center">${parseFloat(((grade.toFixed(2)/weight)*100).toFixed(2))}%</td>
+   		<td align="center">${calculateLetterGrade((grade/weight)*100)}</td>
+   		<td width="19"><img class="indicator" style="display:none" src="/images/icon_check.gif"></td>
+		<td width="19"><img class="indicator" style="display:none" src="/images/icon_late.gif"></td>
+		<td width="19"><img class="indicator" style="display:none" src="/images/icon_missing.gif"></td>
+		<td width="19"><img class="indicator" style="display:none" src="/images/icon_exempt.gif"></td>
+		<td width="19"><img class="indicator" style="display:none" src="/images/icon_excluded.gif"></td></tr>
+   	`;
+});
+
 const ComboBox = require('./lib/ComboBox.min.js');
+
+// Experimental calculator class
+class NewCalculator {
+   constructor(strategy, currentGrade, gradeContainer, addedGradesContainer) {
+      this.strategy = strategy;
+      this.currentGrade = currentGrade;
+      this.activeGrades = this.getExistingGrades();
+      this.allGrades = this.getExistingGrades();
+      this.gradeContainer = gradeContainer;
+      this.addedGradesContainer = addedGradesContainer;
+      this.shouldAttach = true;
+   }
+
+   getExistingGrades() {
+      let elements = document.querySelectorAll('#assignmentScores > table > tbody > tr'),
+         out = [];
+      [].forEach.call(elements, (e) => {
+         if(e.children.length > 8 && !isNaN(parseFloat(getNumbersInString(e.children[5].innerText)))) {
+            out.push({
+               weight: this.strategy.determineWeight(e),
+               grade: this.strategy.determineGrade(e)
+            });
+         }
+      });
+      return out;
+   }
+
+   idIsUsed(id) {
+      let hit = false;
+      this.activeGrades.forEach((e) => {
+         if(e.id === id) {
+            hit = true;
+         }
+      });
+      return hit;
+   }
+
+   generateId(l) {
+      const a = 'abcdefghijklmnopqrstuvwxyz0123456789'.split('');
+      let t = '';
+      for(let b = 0; b < l; b++) {
+         t += a[Math.floor(Math.random() * a.length)];
+      }
+      if(this.idIsUsed(t)) {
+         t = this.generateId(l);
+      }
+      return t;
+   }
+
+   displayGrade() {
+      this.gradeContainer.innerHTML = `(${calculateLetterGrade(this.currentGrade)}&nbsp;&nbsp;&nbsp;${this.currentGrade}%)&nbsp;&nbsp;&nbsp;`;
+   }
+
+   disableGrade(id) {
+      this.activeGrades.forEach((e, i) => {
+         if(e.id === id) {
+            this.activeGrades.splice(i, 1);
+         }
+      });
+   }
+
+   enableGrade(id) {
+      this.allGrades.forEach((e) => {
+         if(e.id === id) {
+            this.activeGrades.push(e);
+         }
+      });
+   }
+
+   calculateGrade() {
+      this.currentGrade = this.strategy.calculate(this.activeGrades);
+      this.displayGrade();
+   }
+
+   showAddedGrade(weight, grade, id) {
+      const createdTr = document.createElement('tr');
+      createdTr.innerHTML = this.strategy.determineGradeDisplay(weight, grade);
+      this.addedGradesContainer.appendChild(createdTr);
+
+      createdTr.dataset.id = id;
+
+      createdTr.classList.add('clickable');
+
+      if(this.addedGradesContainer.children.length % 2 === 1) {
+         createdTr.classList.add('oddRow')
+      }
+
+      createdTr.dataset.active = 'true';
+      createdTr.addEventListener('click', () => {
+         if(createdTr.dataset.active === 'true') {
+            createdTr.classList.add('faded');
+            createdTr.dataset.active = 'false';
+            this.disableGrade(createdTr.dataset.id);
+         } else {
+            createdTr.classList.remove('faded');
+            createdTr.dataset.active = 'true';
+            this.enableGrade(createdTr.dataset.id);
+         }
+         this.calculateGrade();
+      });
+   }
+
+   addNewGrade(weight, grade) {
+      const newGrade = {
+         weight: weight,
+         grade: grade,
+         id: this.generateId(8)
+      };
+      this.allGrades.push(newGrade);
+      this.activeGrades.push(newGrade);
+      this.calculateGrade();
+      this.showAddedGrade(weight, grade, newGrade.id);
+   }
+
+   attach(elem, weight, grade, calc) {
+      elem.addEventListener('click', () => {
+         if(this.shouldAttach) {
+            if((weight.value !== '' && grade.value !== '') && !isNaN(parseFloat(weight.value))) {
+               calc.addNewGrade(parseFloat(weight.value), parseFloat(grade.value));
+            } else {
+               alert('Please correct your inputs');
+            }
+         }
+      });
+      weight.addEventListener('keydown', (e) => {
+         if(e.keyCode === 13 && this.shouldAttach) {
+            elem.click();
+         }
+      });
+      grade.addEventListener('keydown', (e) => {
+         if(e.keyCode === 13 && this.shouldAttach) {
+            elem.click();
+         }
+      });
+   }
+   detach() {
+      this.shouldAttach = false;
+      [].forEach.call(document.querySelectorAll('tr[data-id'), (e) => {
+         e.parentNode.removeChild(e);
+      });
+      this.gradeContainer.innerHTML = '';
+   }
+}
 
 // The main calculator class
 class Calculator {
@@ -247,6 +456,12 @@ function InitCalc() {
       }
       comboBoxElem.appendChild(newWeightInput);
       comboBoxElem.appendChild(newWeightSelect);
+      const totalPointsLabel = document.createElement('label');
+      totalPointsLabel.innerHTML = `Total points: `;
+      totalPointsLabel.setAttribute('for', 'totalPointsCheckbox');
+      const totalPointsCheckbox = document.createElement('input');
+      totalPointsCheckbox.id = 'totalPointsCheckbox';
+      totalPointsCheckbox.type = 'checkbox';
       const newGradeInput = document.createElement('input');
       newGradeInput.placeholder = 'Grade';
       newGradeInput.id = 'newGradeInput';
@@ -254,11 +469,14 @@ function InitCalc() {
 
       // Parent element for calculator
       const parentTr = document.createElement('tr');
-      parentTr.innerHTML = `
-    		<td>${addGradeButton.outerHTML}</td>
-    		<td>${comboBoxElem.outerHTML}</td>
-    		<td>${newGradeInput.outerHTML}</td>
-    	`;
+      parentTr.appendChild(document.createElement('td'));
+      parentTr.appendChild(document.createElement('td'));
+      parentTr.appendChild(document.createElement('td'));
+      parentTr.children[0].appendChild(addGradeButton);
+      parentTr.children[1].appendChild(comboBoxElem);
+      parentTr.children[1].appendChild(totalPointsLabel);
+      parentTr.children[1].appendChild(totalPointsCheckbox);
+      parentTr.children[2].appendChild(newGradeInput);
 
       // Insert the parent element
       parent.insertBefore(parentTr, parent.children[0]);
@@ -267,7 +485,8 @@ function InitCalc() {
       document.querySelectorAll('tr.center > td')[3].innerHTML += `<br><div id='recalculatedGrade'></div>`;
 
       // Initialize the calculator
-      const calc = new Calculator(
+      let calc = new NewCalculator(
+         WeightedCalculator,
          parseFloat(getNumbersInString(document.querySelectorAll('tbody')[0].children[1].children[3].innerText)).toFixed(1),
          document.getElementById('recalculatedGrade'),
          document.querySelectorAll('tbody')[1]
@@ -278,6 +497,28 @@ function InitCalc() {
          document.getElementById('newGradeInput'),
          calc
       );
+
+      totalPointsCheckbox.addEventListener('change', () => {
+         calc.detach();
+
+         calc = new NewCalculator(
+            (totalPointsCheckbox.checked ? TotalPointCalculator : WeightedCalculator),
+            parseFloat(getNumbersInString(document.querySelectorAll('tbody')[0].children[1].children[3].innerText)).toFixed(1),
+            document.getElementById('recalculatedGrade'),
+            document.querySelectorAll('tbody')[1]
+         );
+
+         calc.attach(document.getElementById('addGradeButton'),
+            document.getElementById('newWeightInput'),
+            document.getElementById('newGradeInput'),
+            calc
+         );
+
+         newWeightInput.placeholder = (totalPointsCheckbox.checked ? 'Points' : 'Weight');
+
+         newWeightInput.value = '';
+         newGradeInput.value = '';
+      });
 
       let combobox = new ComboBox(document.getElementById('combobox'), false);
    }
